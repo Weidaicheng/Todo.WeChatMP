c2950c08bc930999eee61c646e946e991ba55f89
Weidaicheng <wdcdavyc@outlook.com>
2017-09-08 13:43:46 +0800

    登录方法加入Promiss
    调用方法通过then关联后续操作

--- app.js
+++ app.js
@@ -2,101 +2,115 @@
 App({
   //登录获取UserId
   getUserId: function () {
-    wx.getStorage({
-      key: 'UserId',
-      success: function (resSaved) {
-        //读取UserId成功
-        console.log("读取UserId成功");
-        //验证UserId是否过期
-        console.log("验证UserId是否过期");
-        wx.request({
-          url: 'https://89286074.qcloud.la/api/WeChatLogin/Login',
-          method: "post",
-          data: {
-            UserId: resSaved.data
-          },
-          success: function (resSucess) {
-            //服务器请求成功
-            console.log("服务器请求成功");
-            if (resSucess.data.ErrCode == 0) {
-              //UserId未过期，继续执行操作
-              console.log("UserId未过期，继续执行操作");
-            }
-            else {
-              //UserId过期，重新登录，重新获取UserId
-              console.log("UserId过期，重新登陆，重新获取UserId");
-              wx.login({
-                success: resLogin => {
-                  wx.request({
-                    url: 'https://89286074.qcloud.la/api/WeChatLogin/Login',
-                    method: "post",
-                    data: {
-                      Code: resLogin.code
-                    },
-                    success: function (resSucess) {
-                      //服务器请求成功
-                      console.log("服务器请求成功");
-                      if (resSucess.data.ErrCode == 0) {
-                        //获取UserId成功
-                        console.log("获取UserId成功");
-                        //保存UserId
-                        console.log("保存UserId");
-                        wx.setStorage({
-                          key: 'UserId',
-                          data: resSucess.data.Data,
-                        });
+    return new Promise(function(resolve, reject){
+      wx.getStorage({
+        key: 'UserId',
+        success: function (resSaved) {
+          //读取UserId成功
+          console.log("读取UserId成功");
+          //验证UserId是否过期
+          console.log("验证UserId是否过期");
+          wx.request({
+            url: 'https://89286074.qcloud.la/api/WeChatLogin/Login',
+            method: "post",
+            data: {
+              UserId: resSaved.data
+            },
+            success: function (resSucess) {
+              //服务器请求成功
+              console.log("服务器请求成功");
+              if (resSucess.data.ErrCode == 0) {
+                //UserId未过期，继续执行操作
+                console.log("UserId未过期，继续执行操作");
+                resolve();
+              }
+              else {
+                //UserId过期，重新登录，重新获取UserId
+                console.log("UserId过期，重新登陆，重新获取UserId");
+                wx.login({
+                  success: resLogin => {
+                    wx.request({
+                      url: 'https://89286074.qcloud.la/api/WeChatLogin/Login',
+                      method: "post",
+                      data: {
+                        Code: resLogin.code
+                      },
+                      success: function (resSucess) {
+                        //服务器请求成功
+                        console.log("服务器请求成功");
+                        if (resSucess.data.ErrCode == 0) {
+                          //获取UserId成功
+                          console.log("获取UserId成功");
+                          //保存UserId
+                          console.log("保存UserId");
+                          wx.setStorage({
+                            key: 'UserId',
+                            data: resSucess.data.Data,
+                          });
+                          resolve();
+                        }
+                        else{
+                          reject(resSucess.data.ErrMsg);
+                        }
+                      },
+                      fail: function (resFail) {
+                        //服务器请求失败
+                        console.log("服务器请求失败");
+                        reject("服务器请求失败");
                       }
-                    },
-                    fail: function (resFail) {
-                      //服务器请求失败
-                      console.log("服务器请求失败");
-                    }
-                  });
+                    });
+                  }
+                });
+              }
+            },
+            fail: function (resFail) {
+              //服务器请求失败
+              console.log("服务器请求失败");
+              reject("服务器请求失败");
+            }
+          });
+        },
+        fail: function () {
+          //读取UserId失败
+          console.log("读取UserId失败");
+          //重新登录，获取UserId
+          console.log("重新登录，获取UserId");
+          wx.login({
+            success: resLogin => {
+              wx.request({
+                url: 'https://89286074.qcloud.la/api/WeChatLogin/Login',
+                method: "post",
+                data: {
+                  Code: resLogin.code
+                },
+                success: function (resSucess) {
+                  //服务器请求成功
+                  console.log("服务器请求成功");
+                  if (resSucess.data.ErrCode == 0) {
+                    //获取UserId成功
+                    console.log("获取UserId成功");
+                    //保存UserId
+                    console.log("保存UserId");
+                    wx.setStorage({
+                      key: 'UserId',
+                      data: resSucess.data.Data,
+                    });
+                    resolve();
+                  }
+                  else {
+                    reject(resSucess.data.ErrMsg);
+                  }
+                },
+                fail: function (resFail) {
+                  //服务器请求失败
+                  console.log("服务器请求失败");
+                  reject("服务器请求失败");
                 }
               });
             }
-          },
-          fail: function (resFail) {
-            //服务器请求失败
-            console.log("服务器请求失败");
-          }
-        });
-      },
-      fail: function () {
-        //读取UserId失败
-        console.log("读取UserId失败");
-        //重新登录，获取UserId
-        console.log("重新登录，获取UserId");
-        wx.login({
-          success: resLogin => {
-            wx.request({
-              url: 'https://89286074.qcloud.la/api/WeChatLogin/Login',
-              method: "post",
-              data: {
-                Code: resLogin.code
-              },
-              success: function (resSucess) {
-                //服务器请求成功
-                console.log("服务器请求成功");
-                if (resSucess.data.ErrCode == 0) {
-                  //获取UserId成功
-                  console.log("获取UserId成功");
-                  //保存UserId
-                  console.log("保存UserId");
-                  wx.setStorage({
-                    key: 'UserId',
-                    data: resSucess.data.Data,
-                  });
-                }
-              },
-              fail: function (resFail) {
-                //服务器请求失败
-                console.log("服务器请求失败");
-              }
-            });
-          }
-        });
-      }
+          });
+        }
+      });
     });
   },
 
@@ -106,7 +120,11 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs);
     
-    this.getUserId();
+    this.getUserId().then(function(result){
+      console.log("login success");
+    }, function(err){
+      console.log("login fail");
+    });
 
     // 获取用户信息
     wx.getSetting({
